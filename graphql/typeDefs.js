cf68820808a001efe24808565be02a9d14f38652
Ankit Giri <angiri@microsoft.com>
2020-10-20 18:39:29 +0530

    Register and Login users

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,7 +7,28 @@ module.exports = gql`
         createdAt: String!
         username: String!
     }
+    # the exclamation mark does not make it mandatory for the user. It makes it mandatory for the resolver.
+    type User{
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
+        createdAt: String!
+    }
+
+    # given as an input to resolver for it to return something for us
+    input RegisterInput{
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
     type Query{
         getPosts: [Post]
+    }
+    # make or change our database
+    type Mutation{
+        register(registerInput: RegisterInput): User!
+        login(username: String!, password: String!): User!
     }
 `;
